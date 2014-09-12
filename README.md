9a27795e6823250ec41565380b459bbe376ef73e
Massimo Santini <santini@di.unimi.it>
2014-09-12 10:46:17 +0200

    Added GLP and a README

---
+++ README.md
@@ -0,0 +1,12 @@
+Java 4 Informatica Musicale (j4im)
+==================================
+
+Libreria di supporto all'insegnamento di
+[Programmazione](http://boldi.di.unimi.it/Corsi/Mus2014/) del corso di studi
+in [Informatica
+musicale](http://www.ccdinf.unimi.it/it/corsiDiStudio/2015/F3Xof2/)
+dell'[Università degli Studi di Milano](http://www.unimi.it/).
+
+Gli studenti possono scaricare l'ultima versione dalla pagina delle
+[releases](https://github.com/mapio/j4im/releases); chiunque voglia
+contribuire allo sviluppo può
--- src/it/unimi/di/j4im/esempi/Groove.java
+++ src/it/unimi/di/j4im/esempi/Groove.java
@@ -1,5 +1,25 @@
 package it.unimi.di.j4im.esempi;
 
+/*
+ * Copyright 2014 Massimo Santini
+ * 
+ * This file is part of j4im.
+ *
+ * j4im is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * j4im is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with j4im.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 import it.unimi.di.j4im.notazione.Nota;
 import it.unimi.di.j4im.riproduzione.Batteria;
 import it.unimi.di.j4im.riproduzione.Batteria.Pezzo;
