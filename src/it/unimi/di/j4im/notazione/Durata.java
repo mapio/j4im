0428463647c0635996aa3048f905a486b32f6f6a
Amerigo Prato <amerigo.prato@gmail.com>
2014-12-17 22:04:16 +0100

    Fixing attempt #1

--- src/it/unimi/di/j4im/notazione/Durata.java
+++ src/it/unimi/di/j4im/notazione/Durata.java
@@ -41,11 +41,15 @@ public class Durata implements Comparable <Durata>{
 	 * @throws IllegalArgumentException se numeratore, o denominatore sono
 	 *             negativi, o nulli.
 	 */
-	public Durata( final int numeratore, final int denominatore ) {
+	 public Durata( final int numeratore, final int denominatore ) {
+	 	int x = numeratore;
+		int y = denominatore;
 		if ( numeratore <= 0 || denominatore <= 0 )
 			throw new IllegalArgumentException( "Non sono possibili durate non positive, o il denominatore nullo." );
-		this.numeratore/=Durata.mcd(numeratore, denominatore);
-		this.denominatore/=Durata.mcd(numeratore, denominatore);
+		this.numeratore = numeratore;
+		this.denominatore = denominatore;
+		this.numeratore=x/Durata.mcd(x, y);
+		this.denominatore=y/Durata.mcd(x, y);
 	}
 
 	/**
